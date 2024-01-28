031b65ea32673c3e2351b55392cd6fb01e80e0ee
TeddyIlTed <129688045+TeddyIlTed@users.noreply.github.com>
2024-01-28 20:49:29 +0100

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,19 +1,19 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, filedialog, simpledialog
+from tkcalendar import DateEntry
 from excel_manager import ExcelManager
 from service_number import ServiceNumberManager
-from tkinter import filedialog
-import os 
+import logging
+
 class GestoreFoglioDiMarciaApp:
     def __init__(self, master):
         self.master = master
+        self.master.title("Gestore Foglio di Marcia")
         self.excel_manager = ExcelManager()
         self.service_number_manager = ServiceNumberManager()
         self.create_interface()
 
     def create_interface(self):
-        self.master.title("Gestore Foglio di Marcia")
-
         # Creazione dei campi di input
         self.inputs = {}
         labels = [
@@ -36,6 +36,10 @@ class GestoreFoglioDiMarciaApp:
                 dropdown = tk.OptionMenu(self.master, self.inputs[text], *self.excel_manager.mezzi)
                 dropdown.grid(row=idx, column=1, sticky="ew")
                 dropdown.config(width=20)
+            elif text == "Data (formato DD/MM/YY)":
+                self.inputs[text] = DateEntry(self.master, width=17, background='darkblue',
+                                              foreground='white', borderwidth=2, date_pattern='dd/mm/y')
+                self.inputs[text].grid(row=idx, column=1, sticky="ew")
             else:
                 self.inputs[text] = tk.Entry(self.master)
                 self.inputs[text].grid(row=idx, column=1, sticky="ew")
@@ -46,49 +50,34 @@ class GestoreFoglioDiMarciaApp:
 
         self.master.grid_columnconfigure(1, weight=1)
 
-    def ask_excel_save_location(self):
-        save_path = filedialog.asksaveasfilename(
-            defaultextension=".xlsx",
-            filetypes=[("Excel files", "*.xlsx"), ("All files", "*.*")],
-            title="Salva come"
-        )
-        if save_path:
-            self.excel_manager.set_file_path(save_path)
-        else:
-            messagebox.showerror("Errore", "È necessario scegliere un percorso per salvare il file Excel.")
-
     def save_data(self):
-        data = {label: self.inputs[label].get() for label in self.inputs}
-        data['Numero servizio'] = self.service_number_manager.service_number
+        # Validazione e salvataggio dei dati
+        try:
+            data = {label: self.inputs[label].get() for label in self.inputs}
+            data['Numero servizio'] = self.service_number_manager.service_number
+            self.validate_data(data)
+            self.excel_manager.add_data_to_sheet(data)
+            self.service_number_manager.increment_and_save()
+            self.reset_fields()
+            messagebox.showinfo("Successo", "I dati sono stati salvati con successo.")
+        except Exception as e:
+            logging.error(f"Errore nel salvataggio dei dati: {e}")
+            messagebox.showerror("Errore", "Si è verificato un errore durante il salvataggio dei dati.")
 
+    def validate_data(self, data):
+        # Qui puoi aggiungere ulteriori controlli di validazione
         if not data["Data (formato DD/MM/YY)"]:
-            messagebox.showerror("Errore", "Il campo 'Data' è obbligatorio.")
-            return
+            raise ValueError("Il campo 'Data' è obbligatorio.")
         if not data["Mezzo"]:
-            messagebox.showerror("Errore", "Selezionare un mezzo.")
-            return
-
-        if not os.path.exists(self.excel_manager.file_path):
-            save_path = self.ask_excel_save_location()
-            if save_path:
-                self.excel_manager.file_path = save_path
-            else:
-                messagebox.showerror("Errore", "È necessario scegliere un percorso per salvare il file Excel.")
-                return
-
-        self.excel_manager.add_data_to_sheet(data)
-        self.service_number_manager.increment_and_save()
-        self.reset_fields()
-        messagebox.showinfo("Successo", "I dati sono stati salvati con successo.")
-
-
+            raise ValueError("Selezionare un mezzo.")
 
     def reset_fields(self):
         for label, input_field in self.inputs.items():
             if isinstance(input_field, tk.Entry):
                 input_field.delete(0, tk.END)
             elif isinstance(input_field, tk.StringVar) and label == "Mezzo":
-                input_field.set('') 
+                input_field.set('')
+
     def run(self):
         self.master.mainloop()
 
